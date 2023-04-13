1950c8b50bb0664f57c0cae6b826b08807dc0a20
mateus.goncalves1 <mgoncalves@grupo-artico.com>
2023-04-13 16:32:59 -0300

    [add] :sparkles: add alert audio

--- src/routes/tag.ts
+++ src/routes/tag.ts
@@ -1,38 +1,34 @@
 import { Router, Request, Response } from "express";
 import itemService from "../services/itensService";
-import { Responses } from "../utils";
+import { playAudio, Responses } from "../utils";
 import socket from "../socket";
 
 const router = Router();
 
 const item = new itemService();
 
-router.get('/', async (req: Request, res: Response) => {
-    const { tag, antenna } = req.body;
+router.get("/", async (req: Request, res: Response) => {
+  const { tag, antenna } = req.body;
 
-    const result = await item.verifyRoom(tag, antenna);
+  const result = await item.verifyRoom(tag, antenna);
 
-    Responses(
-        res,
-        result.status,
-        result.message,
-        result.data
-    );
+  Responses(res, result.status, result.message, result.data);
 
-    const permission = result.data?.status;
+  const permission = result.data?.status;
 
-    if(!permission) {
-        socket.sendMessage('Alarm', {
-            title: 'Falha de Permissão',
-            message: 'Tag fora da área permitida. Favor verificar.',
-            object: result.data?.result
-        })
-    }
+  if (!permission) {
+    socket.sendMessage("Alarm", {
+      title: "Falha de Permissão",
+      message: "Tag fora da área permitida. Favor verificar.",
+      object: result.data?.result,
+    });
+    playAudio();
+  }
 
-    socket.sendMessage('Tag', {
-        title: 'Localização da Tag',
-        object: result.data?.result
-    })
+  socket.sendMessage("Tag", {
+    title: "Localização da Tag",
+    object: result.data?.result,
+  });
 });
 
-export { router as routerTag }
+export { router as routerTag };
