87a5ef2529b4174e54b58c30cc5c3b43b81fe20d
acehrls <chailton@ext.grupo-artico.com>
2023-04-13 11:24:07 -0300

    Criado arquivo de rotas / Inclusão de rotas iniciais

---
+++ src/routes/item.ts
@@ -0,0 +1,25 @@
+import { Request, Response, Router } from "express";
+
+const router = Router();
+
+router.post('/create_item', (req: Request, res: Response) => {
+
+});
+
+router.post('/create_room', (req: Request, res: Response) => {
+
+});
+
+router.get('/get_item', (req: Request, res: Response) => {
+
+});
+
+router.get('/get_room', (req: Request, res: Response) => {
+
+});
+
+router.get('/verify_room', (req: Request, res: Response) => {
+
+});
+
+export { router as routerItem }
