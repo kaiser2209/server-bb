4a23335c827691dd859dce68481ccb593e8ae496
acehrls <chailton@ext.grupo-artico.com>
2023-04-13 13:26:17 -0300

    Implementação da rota de criação de quartos

--- src/routes/item.ts
+++ src/routes/item.ts
@@ -1,13 +1,35 @@
-import { Request, Response, Router } from "express";
+import { Request, Response, Router, response } from "express";
+import itemService from "../services/itensService";
+import roomService from "../services/roomsService";
+import { v4 } from 'uuid';
+import { Responses } from "../utils";
+import { IResponse } from "../interfaces";
 
 const router = Router();
+
+const item = new itemService();
+const room = new roomService();
 
 router.post('/create_item', (req: Request, res: Response) => {
 
 });
 
-router.post('/create_room', (req: Request, res: Response) => {
+router.post('/create_room', async (req: Request, res: Response) => {
+    const { name, antenna } = req.body;
 
+    const result: IResponse = await room.createRoom({
+        name,
+        antenna,
+        id: v4(),
+        createdAt: (new Date()).toISOString()
+    });
+
+    Responses(
+        res,
+        result.status,
+        result.message,
+        result.data
+    )
 });
 
 router.get('/verify_room', (req: Request, res: Response) => {
